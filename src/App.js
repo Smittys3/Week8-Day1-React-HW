17d8abcb5d029441dfe6aeb6771d80b9e1c4f4be
Smittys3 <ssmith@me.com>
2022-08-16 14:55:34 -0500

    Week8-React-Day1-HW

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Contact from './Contact';
+import Home from './Home';
+import Nav from './Nav';
+import Register from './Register';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      user: {},
+      name: 'Stephen',
+      age: 100
+    }
+  }
+
+  subtractFromAge = () => {
+    this.setState({age: this.state.age - 1}) 
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Nav />
+        </div>
+        <header className="App-header">
+          <div>
+            
+            
+            <button onClick={this.subtractFromAge} type="button" class="btn btn-info">Countdown</button>
+
+            
+            {/* {BLOCK CONTENT} */}
+            <Routes>
+              <Route path='/' element={<Home name={this.state.name}  age={this.state.age}/>}/>
+              <Route path='/contact' element={<Contact/>}/>
+              <Route path='/register' element={<Register/>}/>
+            </Routes>
+            {/* {BLOCK CONTENT} */}
+
+          </div>
+        </header>
+      </BrowserRouter>
+    )
+  }
 }
 
-export default App;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <>
+        <br/>
+          {this.props.name} <br/>
+          {this.props.age}
+      </>
+    )
+  }
+}
