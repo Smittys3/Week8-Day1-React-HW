65991eb0131040f985c4544a57328b3d32613177
Smittys3 <ssmith@me.com>
2022-08-18 10:34:53 -0500

    ToDoList

--- src/Nav.js
+++ src/Nav.js
@@ -19,6 +19,9 @@ export default class Nav extends Component {
                 <Link className="nav-link" to="/contact">Contact</Link>
               </li>
               <li className="nav-item">
+                <Link className="nav-link" to="/todolist">ToDoList</Link>
+              </li>
+              <li className="nav-item">
                 <Link className="nav-link" to="/register">Register</Link>
               </li>
               <li className="nav-item dropdown">
